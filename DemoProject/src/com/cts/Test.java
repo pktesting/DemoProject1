67adf6eaa9c32307247b6b323dd3860bb43b1b58
CGI <CGI@CGI-JAVA-092>
2019-05-09 11:25:34 +0530

    Added one more print statement

--- DemoProject/src/com/cts/Test.java
+++ DemoProject/src/com/cts/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("Hello Git 1");
+		System.out.println("Hello Git 2");
 	}
 
 }
